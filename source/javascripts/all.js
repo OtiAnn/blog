68fbd110d0401354245d7ceac94cfccdce2a5cae
OtiAnn <anniebh30@gmail.com>
2015-01-01 18:57:40 +0800

    Fix js, css

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,17 @@
 //= require_tree .
+$().ready(function(){
+
+	var navbarYOffset = $('#navbar').offset().top;
+	$(window).scroll(function(){
+	  if(window.scrollY > navbarYOffset){
+	  	$('header').addClass('fixed-on-top');
+	  	$('.container').addClass('re-position-for-navbar')
+	  	$('footer').addClass('re-position-for-content')
+	  }else{
+	  	$('header').removeClass('fixed-on-top');
+	    $('.container').removeClass('re-position-for-navbar')
+	  	$('footer').removeClass('re-position-for-content')
+	  }
+	});
+	
+})
