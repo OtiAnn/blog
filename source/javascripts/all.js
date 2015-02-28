768e78d9dc07f61be197b0edc82f2f3722501be3
OtiAnn <anniebh30@gmail.com>
2015-02-28 19:32:53 +0800

    Fix navbar style for mobile

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,12 +5,10 @@ $().ready(function(){
 	$(window).scroll(function(){
 	  if(window.scrollY > navbarYOffset){
 	  	$('header').addClass('fixed-on-top');
-	  	$('.navbar-480').addClass('re-position-for-navbar-480')
 	  	$('.container').addClass('re-position-for-navbar')
 	  	$('footer').addClass('re-position-for-content')
 	  }else{
 	  	$('header').removeClass('fixed-on-top');
-	  	$('.navbar-480').removeClass('re-position-for-navbar-480')
 	    $('.container').removeClass('re-position-for-navbar')
 	  	$('footer').removeClass('re-position-for-content')
 	  }
