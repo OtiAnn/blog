f160ae4a4e50aad7151d8b72914a54fb456784ed
OtiAnn <anniebh30@gmail.com>
2015-03-04 22:50:48 +0800

    Remove fix on top style

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,18 +1,18 @@
 $(document).on('ready page:load', function(){
 
-	var navbarYOffset = $('#navbar').offset().top;
+	// var navbarYOffset = $('#navbar').offset().top;
 
-	$(window).scroll(function(){
-	  if(window.scrollY > navbarYOffset){
-	  	$('header').addClass('fixed-on-top');
-	  	$('.container').addClass('re-position-for-navbar')
-	  	$('footer').addClass('re-position-for-content')
-	  }else{
-	  	$('header').removeClass('fixed-on-top');
-	    $('.container').removeClass('re-position-for-navbar')
-	  	$('footer').removeClass('re-position-for-content')
-	  }
-	});
+	// $(window).scroll(function(){
+	//   if(window.scrollY > navbarYOffset){
+	//   	$('header').addClass('fixed-on-top');
+	//   	$('.container').addClass('re-position-for-navbar')
+	//   	$('footer').addClass('re-position-for-content')
+	//   }else{
+	//   	$('header').removeClass('fixed-on-top');
+	//     $('.container').removeClass('re-position-for-navbar')
+	//   	$('footer').removeClass('re-position-for-content')
+	//   }
+	// });
 	
 	$('.navbar-480').click(function(){
 		$(this).toggleClass('open');
